58c4ef8d88c7aa1fee3311d58fbcf71375555f43
John Paul Veneracion <jpveneracion@gmail.com>
2022-06-15 21:59:06 +0800

    Removed default component and uploaded Header without styles

---
+++ src/components/header/Clock.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import './Clock.css';
+//import { ReactComponent as ClockIcon } from '../images/icons/clock.svg';
+//import { ReactComponent as CalenderIcon } from '../images/icons/calendar.svg';
+
+const Clock = () => {
+    const [dateState, setDateState] = useState(new Date());
+    useEffect(() => {
+           setInterval(() => setDateState(new Date()), 1000);
+    }, []);
+    return (
+        <div className="clock">
+            <p className="clock-day">
+                {dateState.toLocaleDateString('en-US',{
+                    weekday: 'long',
+                })}
+            </p>
+            <p className="clock-date">
+              {dateState.toLocaleDateString('en-US', {
+                 day: 'numeric',
+                 month: 'short',
+                 year: 'numeric',
+              })}
+              
+              {' | '}
+             {dateState.toLocaleString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+                hour12: true,
+            })}
+            </p>
+        </div>
+    );
+}
+
+export default Clock;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/header.png';
+import Clock from './Clock';
+const Header = () => {
+
+    return(
+        <div className='header'>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Clock />
+            <ul className="App-navi">
+                <li>Home</li>
+                <li>All Results</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
