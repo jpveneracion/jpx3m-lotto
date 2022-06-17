3273d7044eb8edd2033c169198d80b47ea7a9bf9
John Paul Veneracion <jpveneracion@gmail.com>
2022-06-17 22:42:07 +0800

    fixed day draws and created necessary components

---
+++ src/components/result/Friday.jsx
@@ -0,0 +1,64 @@
+import FiftyEight from './images/games/658.png';
+import FortyFive from './images/games/645.png';
+import FourD from './images/games/4d.png';
+import ThreeD from './images/games/3d.png';
+import TwoD from './images/games/2d.png';
+
+
+const Friday = () =>{
+    return(
+        <>
+        <div className="result-card">
+            <div>
+                <img src={FiftyEight} alt='lotto game six fifty eight'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        <div className="result-card">
+            <div>
+                <img src={FortyFive} alt='lotto game six forty five'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        <div className="result-card">
+            <div>
+                <img src={FourD} alt='lotto game four digit'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        <div className="result-card">
+            <div>
+                <img src={ThreeD} alt='lotto game three digit'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        <div className="result-card">
+            <div>
+                <img src={TwoD} alt='lotto game two digit'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Friday
---
+++ src/components/result/JackpotGames.jsx
@@ -0,0 +1,64 @@
+import FiftyEight from './images/games/658.png';
+import FiftyFive from './images/games/655.png';
+import FortyNine from './images/games/649.png';
+import FortyFive from './images/games/645.png';
+import FortyTwo from './images/games/642.png';
+
+
+const JackpotGames = () =>{
+    return(
+        <>
+        <div className="result-card">
+            <div>
+                <img src={FiftyEight} alt='lotto game six fifty eight'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        <div className="result-card">
+            <div>
+                <img src={FiftyFive} alt='lotto game six fifty five'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        <div className="result-card">
+            <div>
+                <img src={FortyNine} alt='lotto game six forty nine'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        <div className="result-card">
+            <div>
+                <img src={FortyFive} alt='lotto game six forty five'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        <div className="result-card">
+            <div>
+                <img src={FortyTwo} alt='lotto game six forty'/>
+            </div>
+            <div className='result-details'>
+                <div>Game</div>
+                <div>Date</div>
+                <div>Results</div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default JackpotGames
