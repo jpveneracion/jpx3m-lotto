b492dbc13b5e4fef4eeb3b7e428a819e1c82cd46
jpveneracion <jpveneracion@gmail.com>
2022-06-20 11:28:53 +0800

    Create ResultsData.js

---
+++ src/components/Result/ResultsData.js
@@ -0,0 +1,55 @@
+export default{
+    "data":{
+        "results":{
+            "fiftyEight":[
+            {
+                key: "1",
+                gameName: "58",
+                is2dOr3d: false,
+                winningNumber: ["08", "11", "12", "16", "22", "23"],
+                drawDate: "6/7/2022",
+                isJackpot: true
+            }],
+            "fiftyFive":[
+            {
+                key: "2",
+                gameName: "55",
+                is2dOr3d: false,
+                winningNumber: ["10", "16", "19", "08", "03", "23"],
+                drawDate: "6/8/2022",
+                isJackpot: true
+            }],
+            "42":[
+            {
+                key: "3",
+                gameName: "42",
+                is2dOr3d: false,
+                winningNumber: ["08", "11", "12", "16", "22", "23"],
+                drawDate: "6/7/2022",
+                isJackpot: true
+            }],
+            "6D":[
+            {
+                key: "4",
+                gameName: "6D",
+                is2dOr3d: false,
+                winningNumber: ["8", "1", "2"," 6", "2", "3"],
+                drawDate: "6/7/2022",
+                isJackpot: false
+            }],
+            "3D":[
+            {
+                key: "5",
+                gameName: "3D",
+                is2dOr3d: true,
+                winningNumber: {
+                    "2PM": ["1","7","2"],
+                    "5PM": ["1","7","2"],
+                    "9PM": ["1","7","2"]
+            },
+                drawDate: "6/7/2022",
+                isJackpot: false
+            }]
+        }
+    }
+}
